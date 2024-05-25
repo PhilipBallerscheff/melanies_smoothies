4f5050c64d6f61ac62d0587012dce5e92f759a55
PhilipBallerscheff <167703422+PhilipBallerscheff@users.noreply.github.com>
2024-05-25 11:34:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,8 @@ if ingredients_list:
     st.write(f"Ingredients: {ingredients_string}")
 
     # Fetch details about a fruit from an external API
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    st-subheader(fruit_chosen + ' Nutriotion Information')
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # Create the insert statement
